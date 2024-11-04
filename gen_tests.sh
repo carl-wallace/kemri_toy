982e895fa6f59588a4b1d7f9ae16534993a81648
Carl Wallace <carl@redhoundsoftware.com>
2024-11-04 15:44:43 -0500

    update test script

--- gen_tests.sh
+++ gen_tests.sh
@@ -1,16 +1,16 @@
 ./kemri_toy -o output
-./kemri_toy -o output -c ./output/2.16.840.1.101.3.4.4.1_ML-KEM-512_ee.der --kdf hkdf-sha384
-./kemri_toy -o output -c ./output/2.16.840.1.101.3.4.4.1_ML-KEM-512_ee.der --kdf hkdf-sha512
+./kemri_toy -o output -c ./output/2.16.840.1.101.3.4.4.1_ML-KEM-512_ee.der --kdf hkdf-sha2-384
+./kemri_toy -o output -c ./output/2.16.840.1.101.3.4.4.1_ML-KEM-512_ee.der --kdf hkdf-sha2-512
 ./kemri_toy -o output -c ./output/2.16.840.1.101.3.4.4.1_ML-KEM-512_ee.der --kdf kmac128
 ./kemri_toy -o output -c ./output/2.16.840.1.101.3.4.4.1_ML-KEM-512_ee.der --kdf kmac256
 ./kemri_toy -o output --kem ml-kem768
-./kemri_toy -o output -c ./output/2.16.840.1.101.3.4.4.2_ML-KEM-768_ee.der --kem ml-kem768 --kdf hkdf-sha384
-./kemri_toy -o output -c ./output/2.16.840.1.101.3.4.4.2_ML-KEM-768_ee.der --kem ml-kem768 --kdf hkdf-sha512
+./kemri_toy -o output -c ./output/2.16.840.1.101.3.4.4.2_ML-KEM-768_ee.der --kem ml-kem768 --kdf hkdf-sha2-384
+./kemri_toy -o output -c ./output/2.16.840.1.101.3.4.4.2_ML-KEM-768_ee.der --kem ml-kem768 --kdf hkdf-sha2-512
 ./kemri_toy -o output -c ./output/2.16.840.1.101.3.4.4.2_ML-KEM-768_ee.der --kem ml-kem768 --kdf kmac128
 ./kemri_toy -o output -c ./output/2.16.840.1.101.3.4.4.2_ML-KEM-768_ee.der --kem ml-kem768 --kdf kmac256
 ./kemri_toy -o output --kem ml-kem1024
-./kemri_toy -o output -c ./output/2.16.840.1.101.3.4.4.3_ML-KEM-1024_ee.der --kem ml-kem1024 --kdf hkdf-sha384
-./kemri_toy -o output -c ./output/2.16.840.1.101.3.4.4.3_ML-KEM-1024_ee.der --kem ml-kem1024 --kdf hkdf-sha512
+./kemri_toy -o output -c ./output/2.16.840.1.101.3.4.4.3_ML-KEM-1024_ee.der --kem ml-kem1024 --kdf hkdf-sha2-384
+./kemri_toy -o output -c ./output/2.16.840.1.101.3.4.4.3_ML-KEM-1024_ee.der --kem ml-kem1024 --kdf hkdf-sha2-512
 ./kemri_toy -o output -c ./output/2.16.840.1.101.3.4.4.3_ML-KEM-1024_ee.der --kem ml-kem1024 --kdf kmac128
 ./kemri_toy -o output -c ./output/2.16.840.1.101.3.4.4.3_ML-KEM-1024_ee.der --kem ml-kem1024 --kdf kmac256
 ./kemri_toy -i ./output/2.16.840.1.101.3.4.4.1_ML-KEM-512_kemri_id-alg-hkdf-with-sha256.der -k ./output/2.16.840.1.101.3.4.4.1_ML-KEM-512_priv.der
@@ -30,18 +30,18 @@
 ./kemri_toy -i ./output/2.16.840.1.101.3.4.4.3_ML-KEM-1024_kemri_id-kmac256.der -k ./output/2.16.840.1.101.3.4.4.3_ML-KEM-1024_priv.der
 
 ./kemri_toy -o output -u "This is some User Keying Material" -c ./output/2.16.840.1.101.3.4.4.1_ML-KEM-512_ee.der
-./kemri_toy -o output -u "This is some User Keying Material" -c ./output/2.16.840.1.101.3.4.4.1_ML-KEM-512_ee.der --kdf hkdf-sha384
-./kemri_toy -o output -u "This is some User Keying Material" -c ./output/2.16.840.1.101.3.4.4.1_ML-KEM-512_ee.der --kdf hkdf-sha512
+./kemri_toy -o output -u "This is some User Keying Material" -c ./output/2.16.840.1.101.3.4.4.1_ML-KEM-512_ee.der --kdf hkdf-sha2-384
+./kemri_toy -o output -u "This is some User Keying Material" -c ./output/2.16.840.1.101.3.4.4.1_ML-KEM-512_ee.der --kdf hkdf-sha2-512
 ./kemri_toy -o output -u "This is some User Keying Material" -c ./output/2.16.840.1.101.3.4.4.1_ML-KEM-512_ee.der --kdf kmac128
 ./kemri_toy -o output -u "This is some User Keying Material" -c ./output/2.16.840.1.101.3.4.4.1_ML-KEM-512_ee.der --kdf kmac256
 ./kemri_toy -o output -u "This is some User Keying Material" -c ./output/2.16.840.1.101.3.4.4.2_ML-KEM-768_ee.der --kem ml-kem768
-./kemri_toy -o output -u "This is some User Keying Material" -c ./output/2.16.840.1.101.3.4.4.2_ML-KEM-768_ee.der --kem ml-kem768 --kdf hkdf-sha384
-./kemri_toy -o output -u "This is some User Keying Material" -c ./output/2.16.840.1.101.3.4.4.2_ML-KEM-768_ee.der --kem ml-kem768 --kdf hkdf-sha512
+./kemri_toy -o output -u "This is some User Keying Material" -c ./output/2.16.840.1.101.3.4.4.2_ML-KEM-768_ee.der --kem ml-kem768 --kdf hkdf-sha2-384
+./kemri_toy -o output -u "This is some User Keying Material" -c ./output/2.16.840.1.101.3.4.4.2_ML-KEM-768_ee.der --kem ml-kem768 --kdf hkdf-sha2-512
 ./kemri_toy -o output -u "This is some User Keying Material" -c ./output/2.16.840.1.101.3.4.4.2_ML-KEM-768_ee.der --kem ml-kem768 --kdf kmac128
 ./kemri_toy -o output -u "This is some User Keying Material" -c ./output/2.16.840.1.101.3.4.4.2_ML-KEM-768_ee.der --kem ml-kem768 --kdf kmac256
 ./kemri_toy -o output -u "This is some User Keying Material" -c ./output/2.16.840.1.101.3.4.4.3_ML-KEM-1024_ee.der --kem ml-kem1024
-./kemri_toy -o output -u "This is some User Keying Material" -c ./output/2.16.840.1.101.3.4.4.3_ML-KEM-1024_ee.der --kem ml-kem1024 --kdf hkdf-sha384
-./kemri_toy -o output -u "This is some User Keying Material" -c ./output/2.16.840.1.101.3.4.4.3_ML-KEM-1024_ee.der --kem ml-kem1024 --kdf hkdf-sha512
+./kemri_toy -o output -u "This is some User Keying Material" -c ./output/2.16.840.1.101.3.4.4.3_ML-KEM-1024_ee.der --kem ml-kem1024 --kdf hkdf-sha2-384
+./kemri_toy -o output -u "This is some User Keying Material" -c ./output/2.16.840.1.101.3.4.4.3_ML-KEM-1024_ee.der --kem ml-kem1024 --kdf hkdf-sha2-512
 ./kemri_toy -o output -u "This is some User Keying Material" -c ./output/2.16.840.1.101.3.4.4.3_ML-KEM-1024_ee.der --kem ml-kem1024 --kdf kmac128
 ./kemri_toy -o output -u "This is some User Keying Material" -c ./output/2.16.840.1.101.3.4.4.3_ML-KEM-1024_ee.der --kem ml-kem1024 --kdf kmac256
 ./kemri_toy -i ./output/2.16.840.1.101.3.4.4.1_ML-KEM-512_kemri_id-alg-hkdf-with-sha256_ukm.der -k ./output/2.16.840.1.101.3.4.4.1_ML-KEM-512_priv.der
@@ -62,18 +62,18 @@
 
 
 ./kemri_toy -o output --aead aes256-gcm --auth-env-data -c ./output/2.16.840.1.101.3.4.4.1_ML-KEM-512_ee.der
-./kemri_toy -o output -c ./output/2.16.840.1.101.3.4.4.1_ML-KEM-512_ee.der --kdf hkdf-sha384 --aead aes256-gcm --auth-env-data
-./kemri_toy -o output -c ./output/2.16.840.1.101.3.4.4.1_ML-KEM-512_ee.der --kdf hkdf-sha512 --aead aes256-gcm --auth-env-data
+./kemri_toy -o output -c ./output/2.16.840.1.101.3.4.4.1_ML-KEM-512_ee.der --kdf hkdf-sha2-384 --aead aes256-gcm --auth-env-data
+./kemri_toy -o output -c ./output/2.16.840.1.101.3.4.4.1_ML-KEM-512_ee.der --kdf hkdf-sha2-512 --aead aes256-gcm --auth-env-data
 ./kemri_toy -o output -c ./output/2.16.840.1.101.3.4.4.1_ML-KEM-512_ee.der --kdf kmac128 --aead aes256-gcm --auth-env-data
 ./kemri_toy -o output -c ./output/2.16.840.1.101.3.4.4.1_ML-KEM-512_ee.der --kdf kmac256 --aead aes256-gcm --auth-env-data
 ./kemri_toy -o output --kem ml-kem768 --aead aes256-gcm --auth-env-data -c ./output/2.16.840.1.101.3.4.4.2_ML-KEM-768_ee.der
-./kemri_toy -o output -c ./output/2.16.840.1.101.3.4.4.2_ML-KEM-768_ee.der --kem ml-kem768 --kdf hkdf-sha384 --aead aes256-gcm --auth-env-data
-./kemri_toy -o output -c ./output/2.16.840.1.101.3.4.4.2_ML-KEM-768_ee.der --kem ml-kem768 --kdf hkdf-sha512 --aead aes256-gcm --auth-env-data
+./kemri_toy -o output -c ./output/2.16.840.1.101.3.4.4.2_ML-KEM-768_ee.der --kem ml-kem768 --kdf hkdf-sha2-384 --aead aes256-gcm --auth-env-data
+./kemri_toy -o output -c ./output/2.16.840.1.101.3.4.4.2_ML-KEM-768_ee.der --kem ml-kem768 --kdf hkdf-sha2-512 --aead aes256-gcm --auth-env-data
 ./kemri_toy -o output -c ./output/2.16.840.1.101.3.4.4.2_ML-KEM-768_ee.der --kem ml-kem768 --kdf kmac128 --aead aes256-gcm --auth-env-data
 ./kemri_toy -o output -c ./output/2.16.840.1.101.3.4.4.2_ML-KEM-768_ee.der --kem ml-kem768 --kdf kmac256 --aead aes256-gcm --auth-env-data
 ./kemri_toy -o output --kem ml-kem1024 --aead aes256-gcm --auth-env-data -c ./output/2.16.840.1.101.3.4.4.3_ML-KEM-1024_ee.der
-./kemri_toy -o output -c ./output/2.16.840.1.101.3.4.4.3_ML-KEM-1024_ee.der --kem ml-kem1024 --kdf hkdf-sha384 --aead aes256-gcm --auth-env-data
-./kemri_toy -o output -c ./output/2.16.840.1.101.3.4.4.3_ML-KEM-1024_ee.der --kem ml-kem1024 --kdf hkdf-sha512 --aead aes256-gcm --auth-env-data
+./kemri_toy -o output -c ./output/2.16.840.1.101.3.4.4.3_ML-KEM-1024_ee.der --kem ml-kem1024 --kdf hkdf-sha2-384 --aead aes256-gcm --auth-env-data
+./kemri_toy -o output -c ./output/2.16.840.1.101.3.4.4.3_ML-KEM-1024_ee.der --kem ml-kem1024 --kdf hkdf-sha2-512 --aead aes256-gcm --auth-env-data
 ./kemri_toy -o output -c ./output/2.16.840.1.101.3.4.4.3_ML-KEM-1024_ee.der --kem ml-kem1024 --kdf kmac128 --aead aes256-gcm --auth-env-data
 ./kemri_toy -o output -c ./output/2.16.840.1.101.3.4.4.3_ML-KEM-1024_ee.der --kem ml-kem1024 --kdf kmac256 --aead aes256-gcm --auth-env-data
 ./kemri_toy -i ./output/2.16.840.1.101.3.4.4.1_ML-KEM-512_kemri_auth_id-alg-hkdf-with-sha256.der -k ./output/2.16.840.1.101.3.4.4.1_ML-KEM-512_priv.der
@@ -93,18 +93,18 @@
 ./kemri_toy -i ./output/2.16.840.1.101.3.4.4.3_ML-KEM-1024_kemri_auth_id-kmac256.der -k ./output/2.16.840.1.101.3.4.4.3_ML-KEM-1024_priv.der
 
 ./kemri_toy -o output -u "This is some User Keying Material" -c ./output/2.16.840.1.101.3.4.4.1_ML-KEM-512_ee.der --aead aes256-gcm --auth-env-data
-./kemri_toy -o output -u "This is some User Keying Material" -c ./output/2.16.840.1.101.3.4.4.1_ML-KEM-512_ee.der --kdf hkdf-sha384 --aead aes256-gcm --auth-env-data
-./kemri_toy -o output -u "This is some User Keying Material" -c ./output/2.16.840.1.101.3.4.4.1_ML-KEM-512_ee.der --kdf hkdf-sha512 --aead aes256-gcm --auth-env-data
+./kemri_toy -o output -u "This is some User Keying Material" -c ./output/2.16.840.1.101.3.4.4.1_ML-KEM-512_ee.der --kdf hkdf-sha2-384 --aead aes256-gcm --auth-env-data
+./kemri_toy -o output -u "This is some User Keying Material" -c ./output/2.16.840.1.101.3.4.4.1_ML-KEM-512_ee.der --kdf hkdf-sha2-512 --aead aes256-gcm --auth-env-data
 ./kemri_toy -o output -u "This is some User Keying Material" -c ./output/2.16.840.1.101.3.4.4.1_ML-KEM-512_ee.der --kdf kmac128 --aead aes256-gcm --auth-env-data
 ./kemri_toy -o output -u "This is some User Keying Material" -c ./output/2.16.840.1.101.3.4.4.1_ML-KEM-512_ee.der --kdf kmac256 --aead aes256-gcm --auth-env-data
 ./kemri_toy -o output -u "This is some User Keying Material" -c ./output/2.16.840.1.101.3.4.4.2_ML-KEM-768_ee.der --kem ml-kem768 --aead aes256-gcm --auth-env-data
-./kemri_toy -o output -u "This is some User Keying Material" -c ./output/2.16.840.1.101.3.4.4.2_ML-KEM-768_ee.der --kem ml-kem768 --kdf hkdf-sha384 --aead aes256-gcm --auth-env-data
-./kemri_toy -o output -u "This is some User Keying Material" -c ./output/2.16.840.1.101.3.4.4.2_ML-KEM-768_ee.der --kem ml-kem768 --kdf hkdf-sha512 --aead aes256-gcm --auth-env-data
+./kemri_toy -o output -u "This is some User Keying Material" -c ./output/2.16.840.1.101.3.4.4.2_ML-KEM-768_ee.der --kem ml-kem768 --kdf hkdf-sha2-384 --aead aes256-gcm --auth-env-data
+./kemri_toy -o output -u "This is some User Keying Material" -c ./output/2.16.840.1.101.3.4.4.2_ML-KEM-768_ee.der --kem ml-kem768 --kdf hkdf-sha2-512 --aead aes256-gcm --auth-env-data
 ./kemri_toy -o output -u "This is some User Keying Material" -c ./output/2.16.840.1.101.3.4.4.2_ML-KEM-768_ee.der --kem ml-kem768 --kdf kmac128 --aead aes256-gcm --auth-env-data
 ./kemri_toy -o output -u "This is some User Keying Material" -c ./output/2.16.840.1.101.3.4.4.2_ML-KEM-768_ee.der --kem ml-kem768 --kdf kmac256 --aead aes256-gcm --auth-env-data
 ./kemri_toy -o output -u "This is some User Keying Material" -c ./output/2.16.840.1.101.3.4.4.3_ML-KEM-1024_ee.der --kem ml-kem1024 --aead aes256-gcm --auth-env-data
-./kemri_toy -o output -u "This is some User Keying Material" -c ./output/2.16.840.1.101.3.4.4.3_ML-KEM-1024_ee.der --kem ml-kem1024 --kdf hkdf-sha384 --aead aes256-gcm --auth-env-data
-./kemri_toy -o output -u "This is some User Keying Material" -c ./output/2.16.840.1.101.3.4.4.3_ML-KEM-1024_ee.der --kem ml-kem1024 --kdf hkdf-sha512 --aead aes256-gcm --auth-env-data
+./kemri_toy -o output -u "This is some User Keying Material" -c ./output/2.16.840.1.101.3.4.4.3_ML-KEM-1024_ee.der --kem ml-kem1024 --kdf hkdf-sha2-384 --aead aes256-gcm --auth-env-data
+./kemri_toy -o output -u "This is some User Keying Material" -c ./output/2.16.840.1.101.3.4.4.3_ML-KEM-1024_ee.der --kem ml-kem1024 --kdf hkdf-sha2-512 --aead aes256-gcm --auth-env-data
 ./kemri_toy -o output -u "This is some User Keying Material" -c ./output/2.16.840.1.101.3.4.4.3_ML-KEM-1024_ee.der --kem ml-kem1024 --kdf kmac128 --aead aes256-gcm --auth-env-data
 ./kemri_toy -o output -u "This is some User Keying Material" -c ./output/2.16.840.1.101.3.4.4.3_ML-KEM-1024_ee.der --kem ml-kem1024 --kdf kmac256 --aead aes256-gcm --auth-env-data
 ./kemri_toy -i ./output/2.16.840.1.101.3.4.4.1_ML-KEM-512_kemri_auth_id-alg-hkdf-with-sha256_ukm.der -k ./output/2.16.840.1.101.3.4.4.1_ML-KEM-512_priv.der
